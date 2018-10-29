d2c91a38c840ff805b741bde174484125cbacb49
BZXG <1711723933@qq.com>
2018-10-29 22:05:10 +0800

    444

--- src/static/config.js
+++ src/static/config.js
@@ -1,4 +1,15 @@
 var baseUrl='adkjfhalkdjfhlkakhfd';
 var sunhemeng='1234567898978989'
 export default baseUrl;
-
+//var baseUrl='adkjfhalkdjfhlkakhfd';
+var baseUrl='sadasada';
+var lh='1234';
+var chiji='bixude';
+var wangyanlin='tiemenkanbixin';
+var sunhemeng='1324141234'
+var nihao=123;
+var hello=123;
+var lh='hahahaha';
+var hh='123456';
+var qiyou='1111';
+var llll='2222';
